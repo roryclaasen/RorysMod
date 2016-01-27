1e98c26cc4107f961351ecb52329a85b01f9c44a
GOGO98901 <gogo98901@live.co.uk>
2016-01-27 20:40:41 +0000

    Removed tutorial code

--- src/main/java/net/roryclaasen/rorysmod/entity/EntityLaser.java
+++ src/main/java/net/roryclaasen/rorysmod/entity/EntityLaser.java
@@ -1,10 +1,7 @@
 package net.roryclaasen.rorysmod.entity;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockRedstoneDiode;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.projectile.EntityThrowable;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
@@ -18,30 +15,32 @@ public class EntityLaser extends EntityThrowable {
 	}
 
 	public EntityLaser(World world, EntityLivingBase entity) {
-		super(world, entity); //SUPER! WOOOOOOORLD ENTITY!
-		this.posY += entity.height/2;
+		super(world, entity); // SUPER! WOOOOOOORLD ENTITY!
+		this.posY += entity.height / 2;
 	}
 
 	private void explode() {
-		int bx = (int) posX;
-		int by = (int) posY;
-		int bz = (int) posZ;
+		// int bx = (int) posX;
+		// int by = (int) posY;
+		// int bz = (int) posZ;
 		worldObj.createExplosion(this, posX, posY, posZ, 0.75F, true);
 
-		for (int x = bx - empRadius; x <= empRadius; x++) {
-			for (int y = by - empRadius; y <= by + empRadius; y++) {
-				for (int z = bz - empRadius; z <= bz + empRadius; z++) {
-					Block block = worldObj.getBlock(x, y, z);
-					if (block == Blocks.redstone_wire || block instanceof BlockRedstoneDiode) {
-						block.dropBlockAsItem(worldObj, x, y, z, worldObj.getBlockMetadata(x, y, z), 0);
-						worldObj.setBlockToAir(x, y, z);
-					}
-					if (block == Blocks.redstone_block) {
-						worldObj.setBlock(x, y, z, Blocks.stone);
-					}
-				}
-			}
-		}
+		/*
+		 * for (int x = bx - empRadius; x <= empRadius; x++) {
+		 * for (int y = by - empRadius; y <= by + empRadius; y++) {
+		 * for (int z = bz - empRadius; z <= bz + empRadius; z++) {
+		 * Block block = worldObj.getBlock(x, y, z);
+		 * if (block == Blocks.redstone_wire || block instanceof BlockRedstoneDiode) {
+		 * block.dropBlockAsItem(worldObj, x, y, z, worldObj.getBlockMetadata(x, y, z), 0);
+		 * worldObj.setBlockToAir(x, y, z);
+		 * }
+		 * if (block == Blocks.redstone_block) {
+		 * worldObj.setBlock(x, y, z, Blocks.stone);
+		 * }
+		 * }
+		 * }
+		 * }
+		 */
 		setDead();
 	}
 
